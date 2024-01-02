2a5c33a00655b9c3244a4ffb4ebbefb91e5b2a81
Tevin Glover <tevwork.tg@gmail.com>
2024-01-01 21:39:30 -0500

    im at find need to study more

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,17 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
-}
+public class SinglyLinkedList extends LinkedList {
+
+//    public Integer find(int i) {
+//    Integer count = 0;
+//    Object current = ;
+//     while (current != null){
+//         if (current.)
+//     }
+//    }
+//}
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,98 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-/**
- * Created by leon on 1/10/18.
- */
-public class SinglyLinkedListTest {
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+
+class SinglyLinkedListTest {
+
+    @Test
+    public void singlyLinkedListIsNotNullTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+
+        Assert.assertNotNull(singlyLinkedList);
+    }
+    @Test
+    public void singlyLinkedListIsALinkListTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+
+        Assert.assertTrue(singlyLinkedList instanceof LinkedList);
+    }
+    @Test
+    public void isAbleToAddToTheLinkedListTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+        String bill = "Bill";
+        singlyLinkedList.add(bill);
+
+        Integer expected = 1;
+        Integer actual= singlyLinkedList.size();
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void isAbleToRemoveFormTheLinkedListTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+        String bill = "Bill";
+        singlyLinkedList.add(bill);
+        singlyLinkedList.remove(bill);
+
+        Integer expected = 0;
+        Integer actual= singlyLinkedList.size();
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void isTrueContainTheLinkedListTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+        String bill = "Bill";
+        singlyLinkedList.add(bill);
+        String jill = "Jill";
+        singlyLinkedList.add(jill);
+        String dill = "Dill";
+        singlyLinkedList.add(dill);
+        String will = "Will";
+        singlyLinkedList.add(will);
+
+
+        Boolean actual= singlyLinkedList.contains(will);
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void isFalseContainTheLinkedListTest(){
+        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+        String lill = "lill";
+        String bill = "Bill";
+        singlyLinkedList.add(bill);
+        String jill = "Jill";
+        singlyLinkedList.add(jill);
+        String dill = "Dill";
+        singlyLinkedList.add(dill);
+        String will = "Will";
+        singlyLinkedList.add(will);
+
+
+        Boolean actual= singlyLinkedList.contains(lill);
+        Assert.assertFalse(actual);
+    }
+//    @Test
+//    public void FindTheLinkedListTest(){
+//        SinglyLinkedList  singlyLinkedList = new SinglyLinkedList();
+//        String bill = "Bill";
+//        singlyLinkedList.add(bill);
+//        String jill = "Jill";
+//        singlyLinkedList.add(jill);
+//        String dill = "Dill";
+//        singlyLinkedList.add(dill);
+//        String will = "Will";
+//        singlyLinkedList.add(will);
+//
+//       String expected = dill;
+//        String actual= singlyLinkedList.find(3);
+//        Assert.assertEquals(expected,actual);
+//
+//    }
 }
